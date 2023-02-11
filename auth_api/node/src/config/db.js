59e9b147c6faf26f5865d37a91730fe1ef96c85c
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-02-10 18:41:51 -0600

    feat: add files to views page

--- auth_api/node/src/config/db.js
+++ auth_api/node/src/config/db.js
@@ -1,12 +1,13 @@
 let mysql = require('mysql');
 
 let connectionDb = mysql.createConnection({
-    host: 'sre-bootcamp-selection-challenge.cabf3yhjqvmq.us-east-1.rds.amazonaws.com',
-    user: 'secret',
-    password: 'jOdznoyH6swQB9sTGdLUeeSrtejWkcw',
-    database: 'bootcamp_tht',
-    port: 3306
+  host: process.env.DB_HOSTNAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 });
+
 connectionDb.connect(function(err) {
     if (err) {
         console.error('Error de conexion: ' + err.stack);
