f255fc166c2d2ce38b6674e6706bfb64ab6d66b4
Lorenarojas1 <lorena.rojas080912@gmail.com>
2023-02-18 12:37:40 -0600

    refactor: refactor function protected to clear code and show better funcionality

--- auth_api/node/src/routes/index.js
+++ auth_api/node/src/routes/index.js
@@ -1,31 +1,13 @@
 import { health } from '../services/health';
-import { protectFunction } from '../services/protected';
+// import { protectFunction } from '../services/protected';
 import { login } from './login';
-import { pageProtected } from './protected';
+import { protectedRender } from './protected';
 
 export const init = (app) => {
   app.get('/login', (_, res) => {
     res.render('login', { alert: false });
   });
-  app.get(
-    '/protected',
-    (req, res, next) => {
-      const { jwt } = req.cookies;
-      if (jwt) {
-        protectFunction(jwt)
-          .then((user) => {
-            req.user = user;
-            next();
-          })
-          .catch(() => {
-            res.sendStatus(401);
-          });
-      } else {
-        res.sendStatus(401);
-      }
-    },
-    pageProtected
-  );
+  app.get('/protected', protectedRender);
   app.post('/login', login);
   app.get('/health', health);
 };
--- auth_api/node/src/routes/protected.js
+++ auth_api/node/src/routes/protected.js
@@ -1,3 +1,19 @@
-export const pageProtected = async (_, res) => {
-  res.render('protected', { alert: false });
-};
+import { protectFunction } from '../services/protected';
+
+export const protectedRender = async (req, res, next) => {
+    const { jwt } = req.cookies;
+    if (jwt) {
+      protectFunction(jwt)
+        .then((user) => {
+          req.user = user;
+          res.render('protected')
+          next();
+        })
+        .catch(() => {
+          res.sendStatus(403).render('forbidden');
+        });
+    } else {
+      res.status(403).render('forbidden');
+    }
+  }
+
